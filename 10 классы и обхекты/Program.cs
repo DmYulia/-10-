f574858c9ecb9bffcdebcfb80a6f1193f06b6f9b
Юлия <@LAPTOP-B0K7S1UE>
2022-01-24 20:20:44 +0700

    Добавьте файлы проекта.

---
+++ 10 классы и обхекты/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//возьмем только положительные значения углов, минуты и секунды не больше 60.
+namespace _10_классы_и_обхекты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gr, m, s;
+            Console.WriteLine("Введите размерность угла в целых числах:");
+            G:
+            try
+            {
+                Console.Write("Градусы: ");
+                gr = Convert.ToInt32(Console.ReadLine());
+                if (gr<0) throw new Exception("Некорректные данные!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                goto G;
+            }
+            M:
+            try
+            {
+                Console.Write("Минуты: ");
+                m = Convert.ToInt32(Console.ReadLine());
+                if ((m < 0)|(m>60)) throw new Exception("Некорректные данные!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                goto M;
+            }
+            S:
+            try
+            {
+                Console.Write("Секунды: ");
+                s = Convert.ToInt32(Console.ReadLine());
+                if ((s < 0) | (s > 60)) throw new Exception("Некорректные данные!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                goto S;
+            }
+            Ugol ug = new Ugol(gr, m, s);
+            string q = string.Format("{0:f6}", ug.ToRadians());
+            Console.WriteLine($"Введенный угол равен {q} радиан.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 10 классы и обхекты/Ugol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_классы_и_обхекты
+{
+    class Ugol
+    {
+        public int gradus, min, sec;
+        public Ugol(int gradus, int min, int sec)
+        {
+            /*try
+            {
+                if (gradus < 0)
+                {
+                    throw new Exception("Некорректные данные!");
+                }
+                if ((min < 0)|(min > 60))
+                {
+                    throw new Exception("Некорректные данные!"); 
+                }
+                if ((sec < 0) | (sec > 60))
+                {
+                    throw new Exception("Некорректные данные!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+        public double ToRadians()
+        {
+            return (((gradus + min/60.0 + sec/3600.0)*Math.PI)/180.0);
+        }
+
+    }
+}
